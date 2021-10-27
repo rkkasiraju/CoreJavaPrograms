d52853cd6f037dea66cd8b5b7d0190830c8cf84b
radhakrishna <rkkasiraju@gmail.com>
2021-10-27 09:08:16 +0530

    initial commit

---
+++ src/com/rk/corejava/SubstringTest.java
@@ -0,0 +1,13 @@
+package com.rk.corejava;
+
+public class SubstringTest {
+    static int count = 3;
+
+    public static void main(String[] args) {
+        if (count > 3 == false) {
+            count++;
+            System.out.println("hello");
+        }
+        main(null);
+    }
+}
